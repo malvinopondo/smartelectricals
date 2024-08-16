7ac152e00b2c509416b00dc4e8b5953b7da8fd51
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-08-16 22:25:28 +0300

    Added carousel to my home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { HomeComponent } from "./components/home/home.component";
 import { ServicesComponent } from "./components/services/services.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomeComponent, ServicesComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, ServicesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
